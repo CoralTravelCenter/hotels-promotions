022e98747ee731b0294d05de7048e8dd0f7711c8
Mike <mikhail-gankin@yandex.ru>
2023-10-09 00:13:50 +0300

    Update

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,31 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const notcritical = document.querySelector('.contentLayout-BaseContainer .notcritical');
 	const slides = [...document.querySelectorAll('.navigation-list__item')];
 	const nextBtn = document.querySelector('[data-snap-slider-next]');
 	const prevBtn = document.querySelector('[data-snap-slider-prev]');
 	const locations = [...document.querySelectorAll('[data-location]')];
 	const navigation = document.querySelector('.navigation-list');
+	const promotions = document.querySelector('.promotions');
 
 	nextBtn.addEventListener('click', () => {
 		slides[slides.length - 1].scrollIntoView({
 			behavior: 'smooth',
 			inline: 'start',
 			block: "center"
-		})
-	})
+		});
+	});
 
 	prevBtn.addEventListener('click', () => {
 		slides[0].scrollIntoView({
 			behavior: 'smooth',
 			inline: 'start',
 			block: "center"
-		})
-	})
+		});
+	});
 
 
 	navigation.addEventListener('click', (e) => {
 		if (e.target.hasAttribute('data-location')) {
-			locations.forEach(element => { element.classList.remove('js-active') });
+			locations.forEach(element => element.classList.remove('js-active'));
 			e.target.classList.add('js-active');
 			const dataAttr = e.target.getAttribute('data-location');
 			const targetEl = document.querySelector(`[data-name=${dataAttr}]`);
@@ -33,16 +33,52 @@ document.addEventListener("DOMContentLoaded", () => {
 				top: targetEl.offsetTop - 100,
 				left: 0,
 				behavior: 'smooth'
-			})
+			});
 		}
 	});
 
-	window.addEventListener('scroll', () => {
-		let sc = window.scrollY;
-		if (sc >= 103) {
-			notcritical.classList.add('sticky')
-		} else {
-			notcritical.classList.remove('sticky')
+	function modalGenerator(hotel_name, hotel_rating, info) {
+		let template, clone, html, place;
+		template = document.querySelector('#template');
+
+		clone = template.content.cloneNode(true);
+		html = `
+			${hotel_rating}
+			${hotel_name}
+			${info}
+		`;
+		clone.querySelector('.modal-header').insertAdjacentHTML('afterbegin', html);
+
+		place = document.querySelector('.promotions');
+		place.append(clone);
+	}
+
+	function modalToggler() {
+		const myModal = document.querySelector('#promotion-modal');
+		if (myModal !== null) {
+			new bootstrap.Modal(myModal, {
+				keyboard: true,
+			});
+			$("#promotion-modal").on('hidden.bs.modal', () => {
+				myModal.remove();
+			});
 		}
+	}
+
+	// Берем информацию для модалки с карточки
+	function getInfoFromHotel(e) {
+		let target = e.target;
+		if (target.hasAttribute('data-toggle')) {
+			let parent, hotel_name, hotel_rating, info;
+			parent = target.closest('.content');
+			hotel_name = parent.querySelector('h5').outerHTML;
+			hotel_rating = parent.querySelector('.rating').outerHTML;
+			info = parent.querySelector('.info').outerHTML;
+			modalGenerator(hotel_name, hotel_rating, info);
+		}
+	}
+	promotions.addEventListener('click', (e) => {
+		getInfoFromHotel(e);
+		modalToggler();
 	});
 });
