98f68ef4b8240878fa7280bd8b7ad958af07dd50
Mike <50363065+MikeGankin@users.noreply.github.com>
2023-10-12 19:21:43 +0300

    Update-v3

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-	$('[data-module="leftSiblingMenu"]').detach().prependTo('aside');
-	 
+	const WIDJET = document.querySelector('[data-module="leftSiblingMenu"]');
+	const rightCol = document.querySelector('.rightcol');
+	WIDJET.insertAdjacentHTML('beforebegin', css);
+	rightCol.innerHTML = html;
+
+	const slider = document.querySelector('[data-snap-slider]')
 	const slides = [...document.querySelectorAll('.navigation-list__item')];
 	const nextBtn = document.querySelector('[data-snap-slider-next]');
 	const prevBtn = document.querySelector('[data-snap-slider-prev]');
@@ -8,40 +12,76 @@ document.addEventListener("DOMContentLoaded", () => {
 	const navigation = document.querySelector('.navigation-list');
 	const promotions = document.querySelector('.promotions');
 
+
+	// Добавляем кнопки в навигацию когда появляется горизонтальный скролл
+	function addNavigationButtons() {
+		if (slider.clientWidth < slider.scrollWidth) {
+			nextBtn.style.display = 'block';
+			prevBtn.style.display = 'block';
+		} else {
+			nextBtn.style.display = 'none';
+			prevBtn.style.display = 'none';
+		}
+	}
+	addNavigationButtons();
+	window.addEventListener('resize', _.throttle(addNavigationButtons, 1000))
+
 	nextBtn.addEventListener('click', () => {
 		slides[slides.length - 1].scrollIntoView({
 			behavior: 'smooth',
 			inline: 'start',
-			block: "center"
+			block: "nearest"
 		});
 	});
-
 	prevBtn.addEventListener('click', () => {
 		slides[0].scrollIntoView({
 			behavior: 'smooth',
 			inline: 'start',
-			block: "center"
+			block: "nearest"
 		});
 	});
 
-
-	navigation.addEventListener('click', (e) => {
+	// Навигация
+	function navScroller(e) {
 		if (e.target.hasAttribute('data-location')) {
-			locations.forEach(element => element.classList.remove('js-active'));
-			e.target.classList.add('js-active');
 			const dataAttr = e.target.getAttribute('data-location');
-			const targetEl = document.querySelector(`[data-name=${dataAttr}]`);
-			document.querySelector('.promotions').scrollTo({
-				top: targetEl.offsetTop - 100,
-				left: 0,
-				behavior: 'smooth'
-			});
+			const navHeight = document.querySelector('.hotel-promotions nav').clientHeight;
+			const browHeight = document.querySelector('[data-module="searchtabsmain"]').clientHeight;
+			$([document.documentElement, document.body]).animate({
+				scrollTop: _.floor($(`[data-name=${dataAttr}]`).offset().top) - (navHeight + browHeight + 150)
+			}, 300);
 		}
-	});
+	}
+	navigation.addEventListener('click', navScroller);
 
-	function modalGenerator(hotel_name, hotel_rating, info) {
-		let template, clone, html, place;
+	// Создаем разметку, список или текст в зависимости от колличесва символов в описании
+	function descriptionGenerator(modal_body, description) {
+		let descr_length = description.length;
+		let descr_arr = _.split(description, '.');
+		if (descr_length > 140) {
+			const ul = `
+				<ol>
+					${descr_arr.map(item => {
+				if (item != '') {
+					return `<li>${item}</li>`;
+				}
+			}).join('')
+				}
+				</ol>
+			`;
+			modal_body.insertAdjacentHTML('afterbegin', ul);
+		} else {
+			const p = `<p>${description}</p>`;
+			modal_body.insertAdjacentHTML('afterbegin', p);
+		}
+	}
+
+	// Генерируем модалку
+	function modalGenerator(hotel_name, hotel_rating, info, description) {
+		let fragment, template, clone, html, place;
+		fragment = document.createDocumentFragment();
 		template = document.querySelector('#template');
+		place = document.querySelector('body');
 
 		clone = template.content.cloneNode(true);
 		html = `
@@ -50,15 +90,17 @@ document.addEventListener("DOMContentLoaded", () => {
 			${info}
 		`;
 		clone.querySelector('.modal-header').insertAdjacentHTML('afterbegin', html);
+		descriptionGenerator(clone.querySelector('.modal-body'), description);
 
-		place = document.querySelector('body');
-		place.append(clone);
+		fragment.append(clone);
+		place.append(fragment);
 	}
 
+	// Показываем/скрываем модалку
 	function modalToggler() {
 		const myModal = document.querySelector('#promotion-modal');
 		if (myModal !== null) {
-			$('#promotion-modal').modal({keyboard: true});
+			$('#promotion-modal').modal({ keyboard: true });
 			$("#promotion-modal").on('hidden.bs.modal', () => {
 				myModal.remove();
 			});
@@ -69,16 +111,52 @@ document.addEventListener("DOMContentLoaded", () => {
 	function getInfoFromHotel(e) {
 		let target = e.target;
 		if (target.hasAttribute('data-toggle')) {
-			let parent, hotel_name, hotel_rating, info;
+			let parent, hotel_name, hotel_rating, info, description;
 			parent = target.closest('.content');
 			hotel_name = parent.querySelector('h5').outerHTML;
 			hotel_rating = parent.querySelector('.rating').outerHTML;
 			info = parent.querySelector('.info').outerHTML;
-			modalGenerator(hotel_name, hotel_rating, info);
+			description = parent.querySelector('.description').textContent;
+			modalGenerator(hotel_name, hotel_rating, info, description);
 		}
 	}
 	promotions.addEventListener('click', (e) => {
 		getInfoFromHotel(e);
 		modalToggler();
 	});
+
+	// Генерируем Dropdown на 1024px
+	const mobileWidthMediaQuery = window.matchMedia('(max-width: 1024px)')
+	const dropDownTemplate = document.querySelector('#dropdown').content.cloneNode(true);
+	const place = document.querySelector('.hotel-promotions .navigation');
+	place.append(dropDownTemplate);
+
+	function mobile(isMobileSize) {
+		if (!isMobileSize) {
+			$('[data-module="leftSiblingMenu"]').detach().prependTo('.hotel-promotions aside');
+		} else {
+			$('[data-module="leftSiblingMenu"]').detach().prependTo('.hotel-promotions .navigation .dropdown .dropdown__content')
+		}
+	}
+
+	mobile(mobileWidthMediaQuery.matches)
+	mobileWidthMediaQuery.addEventListener('change', (e) => { mobile(e.matches) })
+
+	// Сраный обзервер
+	// const callback = (entries) => {
+	// 	entries.forEach((entry) => {
+	// 		if (entry.isIntersecting) {
+	// 			console.log(entry.target);
+	// 			locations.forEach(element => element.classList.remove('js-active'));
+	// 			document.querySelector(`[data-location="${entry.target.getAttribute('data-name')}"]`).classList.add('js-active')
+	// 		}
+	// 	})
+	// }
+	// const LISTS = document.querySelectorAll('.promotions-list')
+	// const observer = new IntersectionObserver(callback, {
+	// 	root: document.querySelector('.hotel-promotions'),
+	// 	rootMargin: '-100px',
+	// 	threshold: 0.7
+	// })
+	// LISTS.forEach((el) => observer.observe(el))
 });
