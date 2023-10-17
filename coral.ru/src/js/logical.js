fb8dfa429a34cb9c96473f018a741681d1c12867
Mike <50363065+MikeGankin@users.noreply.github.com>
2023-10-17 19:52:33 +0300

    Parcel Update

--- coral.ru/src/js/logical.js
+++ coral.ru/src/js/logical.js
@@ -1,14 +1,14 @@
-// document.addEventListener("DOMContentLoaded", ()=> {
-// 	function promotionExpiried() {
-// 		const promotions = document.querySelectorAll('[promotion-end]');
-// 		let today = new Date().toLocaleTimeString();
-// 		const dayJs = dayjs();
-// 		console.log(dayJs.duration().asMonths());
-// 		promotions.forEach(promotion => {
-// 			if (today === promotion.getAttribute('promotion-end')) {
-// 				console.log('время вышло');
-// 			}
-// 		});
-// 	}
-// 	promotionExpiried();
-// });
+document.addEventListener("DOMContentLoaded", ()=> {
+	function promotionExpiried() {
+		const promotions = document.querySelectorAll('[promotion-end]');
+		let today = new Date().toLocaleTimeString();
+		const dayJs = dayjs();
+		console.log(dayJs.duration().asMonths());
+		promotions.forEach(promotion => {
+			if (today === promotion.getAttribute('promotion-end')) {
+				console.log('время вышло');
+			}
+		});
+	}
+	promotionExpiried();
+});
