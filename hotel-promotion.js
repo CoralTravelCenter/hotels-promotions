70d1f735988c3d10ceafa3f3d9d68e297ab132f5
Mike <50363065+MikeGankin@users.noreply.github.com>
2023-11-13 19:28:07 +0300

    Update 1.0

--- hotel-promotion.js
+++ hotel-promotion.js
@@ -3,6 +3,7 @@ const PROMOTION_SETTINGS = [
 		name: "Египет",
 		hotels: [
 			{
+				hotel_id: 1,
 				visual: "https://content.coral.ru//resize/800x600/media/image/12/3750/636533875375406850.jpg",
 				rating: 3,
 				hotel_name: "Отель 1",
@@ -25,11 +26,12 @@ const PROMOTION_SETTINGS = [
 				],
 				booking: "20.12.2023",
 				accommodation: "20.12.2023",
-				fly_from: "Все",
+				departure_from: "Все",
 				url: "https://www.coral.ru/hotels/egypt/baron-palace-resort-sahl-hasheesh-hurghada/",
 				promotion_end: ''
 			},
 			{
+				hotel_id: 2,
 				visual: "https://content.coral.ru//resize/800x600/media/image/12/8871/637795739711641789.jpg",
 				rating: 5,
 				hotel_name: "Отель 2",
@@ -52,7 +54,7 @@ const PROMOTION_SETTINGS = [
 				],
 				booking: "До 15 сентября",
 				accommodation: "До 31 октября",
-				fly_from: "Все",
+				departure_from: "Все",
 				url: "https://www.coral.ru/hotels/egypt/movenpick-waterpark-resort-spa-soma-bay-hurghada/",
 				promotion_end: '30.09.2023'
 			}
@@ -62,20 +64,14 @@ const PROMOTION_SETTINGS = [
 		name: "Турция",
 		hotels: [
 			{
+				hotel_id: 3,
 				visual: "https://content.coral.ru//resize/274x172/media/image/12/9117/637815093545591485.jpg",
 				rating: 4,
 				hotel_name: "Отель 3",
 				location: "Хургада (Hurghada), Сал Хашиш (Salh Hasheesh)",
 				description: {
 					text: "Пакет СЕМЕЙНЫЙ 2+2=2 (акция для Deluxe rooms - 2 детей до 11, 99 лет бесплатно (в одной комнате с родителями)). АКЦИЯ для ВСЕХ типов номеров - прекрасная возможность отдохнуть в Спа центре и насладиться пакетом услуг по промо цене!",
-					list: [
-						"сауна (сухой пар) - 15 мин.",
-						"джакузи - 15 мин.",
-						"сауна (хамам) - 15 мин.",
-						"скраб всего тела - 25 мин.",
-						"кокосовая маска на все тело - 25 мин.",
-						"массаж спины - 35 мин."
-					]
+					list: []
 				},
 				conditions: [
 					"Акция распространяется на бронирование полных пакетных туров.",
@@ -84,11 +80,12 @@ const PROMOTION_SETTINGS = [
 				],
 				booking: "20.12.2023",
 				accommodation: "20.12.2023",
-				fly_from: "Все",
+				departure_from: "Все",
 				url: "https://www.coral.ru/hotels/egypt/baron-palace-resort-sahl-hasheesh-hurghada/",
 				promotion_end: ''
 			},
 			{
+				hotel_id: 4,
 				visual: "https://content.coral.ru//resize/274x172/media/image/12/128/637340601982733137.png",
 				rating: 5,
 				hotel_name: "Отель 4",
@@ -111,7 +108,7 @@ const PROMOTION_SETTINGS = [
 				],
 				booking: "До 15 сентября",
 				accommodation: "До 31 октября",
-				fly_from: "Все",
+				departure_from: "Все",
 				url: "https://www.coral.ru/hotels/egypt/movenpick-waterpark-resort-spa-soma-bay-hurghada/",
 				promotion_end: '30.09.2023'
 			}
@@ -133,22 +130,26 @@ function promotionTimer(begin, end) {
 	if (moment(TIME).isAfter(end)) document.querySelector(`[data-end="${end}"]`).remove();
 }
 
-function navigationRender(country) {
+function navigationRender(country_name) {
 	const button = navigation_template.querySelector('button');
 	const data_navigation = document.querySelector('[data-nav]');
-	button.setAttribute('data-location', country.name);
-	button.textContent = country.name;
+	button.setAttribute('data-location', country_name);
+	button.textContent = country_name;
 	const markup = navigation_template.cloneNode(true);
 	data_navigation.append(markup);
 }
 
-function cardRender(country) {
-	const hotel = country.hotels;
+function cardRender(country_name, hotel) {
+	const item = card_template.querySelector('.promotions-list__item');
 	const h5 = card_template.querySelector('h5');
+	const location = card_template.querySelector('[data-location]');
 	const img = card_template.querySelector('.picture img');
 	const rating = card_template.querySelector('.rating');
 	const description = card_template.querySelector('.description');
 	const actions = card_template.querySelector('[data-actions]');
+	const data_booking = card_template.querySelector('[data-booking]');
+	const data_accommodation = card_template.querySelector('[data-accommodation]');
+	const data_departure_from = card_template.querySelector('[data-departure-from]');
 	const star = `
 		<li class="rating__item">
 			<svg width="16" height="16" fill="none">
@@ -156,9 +157,14 @@ function cardRender(country) {
 			</svg>
 		</li>`;
 	hotel.forEach(el => {
+		item.dataset.hotelId = el.hotel_id;
 		rating.innerHTML = star.repeat(el.rating);
 		img.src = el.visual;
 		h5.textContent = el.hotel_name;
+		location.textContent = el.location;
+		data_booking.textContent = el.booking;
+		data_accommodation.textContent = el.accommodation;
+		data_departure_from.textContent = el.departure_from;
 		description.innerHTML = `<p>${el.description.text}</p>`;
 		if (el.description.text.length > 140) {
 			actions.innerHTML = `<a href="${el.url}">ВЫБРАТЬ ТУР</a> <button data-toggle="modal" data-target='promotion-modal'>подробнее</button>`;
@@ -166,29 +172,76 @@ function cardRender(country) {
 			actions.innerHTML = `<a href="${el.url}">ВЫБРАТЬ ТУР</a>`;
 		}
 		const markup = card_template.cloneNode(true);
-		document.querySelector(`[data-name="${country.name}"]`).append(markup);
+		document.querySelector(`[data-name="${country_name}"]`).append(markup);
 	});
 }
 
-function promotionsRender(country) {
+function promotionsRender(country_name) {
 	const ul = country_template.querySelector('ul');
 	const h4 = country_template.querySelector('h4');
-	ul.setAttribute('data-name', country.name)
-	h4.textContent = country.name;
+	ul.setAttribute('data-name', country_name);
+	h4.textContent = country_name;
 	const markup = country_template.cloneNode(true);
 	document.querySelector('.hotel-promotions .promotions').append(markup);
 }
 
-function popupRender(country) {
-	const hotel = country.hotels;
-	hotel.forEach(el=> {
-		
-	})
-}
-
-PROMOTION_SETTINGS.forEach(element => {
-	navigationRender(element);
-	promotionsRender(element);
-	cardRender(element);
-	popupRender(element);
+PROMOTION_SETTINGS.filter(country => {
+	navigationRender(country.name);
+	promotionsRender(country.name);
+	cardRender(country.name, country.hotels);
 });
+
+// Рендерим модалку с описанием акции
+function modalGenerator(target) {
+	let id = parseInt(target.closest('[data-hotel-id]').dataset.hotelId);
+	PROMOTION_SETTINGS.filter(country => {
+		country.hotels.filter(hotel => {
+			if (hotel.hotel_id === id) {
+				const rating = modal_template.querySelector('.rating');
+				const h5 = modal_template.querySelector('h5');
+				const data_booking = modal_template.querySelector('[data-booking]');
+				const data_accommodation = modal_template.querySelector('[data-accommodation]');
+				const data_departure_from = modal_template.querySelector('[data-departure-from]');
+				const data_description = modal_template.querySelector('[data-description]');
+				const star = `
+					<li class="rating__item">
+						<svg width="16" height="16" fill="none">
+							<path fill="#F0AB00" d="m10.758 4.67 4.329.659a.949.949 0 0 1 .48.222.994.994 0 0 1 .294.449 1.028 1.028 0 0 1-.238 1.019l-3.139 3.175.741 4.554c.03.183.01.372-.059.544a.986.986 0 0 1-.33.428.948.948 0 0 1-1.015.072l-3.868-2.127-3.865 2.128a.947.947 0 0 1-1.018-.071.985.985 0 0 1-.328-.43 1.018 1.018 0 0 1-.057-.543l.74-4.554L.286 7.02A1.029 1.029 0 0 1 .048 6a.99.99 0 0 1 .296-.45.945.945 0 0 1 .48-.221l4.325-.659L7.088.56A.98.98 0 0 1 7.443.15a.939.939 0 0 1 1.022 0c.153.099.277.24.356.408l1.937 4.111Z"></path>
+						</svg>
+					</li>
+				`;
+				data_booking.textContent = hotel.booking;
+				data_accommodation.textContent = hotel.accommodation;
+				data_departure_from.textContent = hotel.departure_from;
+				rating.innerHTML = star.repeat(hotel.rating);
+				h5.textContent = hotel.hotel_name;
+
+				if (hotel.description.list.length > 0) {
+					data_description.innerHTML = `
+						<p>${hotel.description.text}</p>
+						<ul class='description-list'>
+							${hotel.description.list.map(item=> {
+								return `<li>${item}</li>`;
+							}).join('')}
+						</ul>
+					`;
+				} else {
+					data_description.innerHTML = `<p>${hotel.description.text}</p>`;
+				}
+
+				const markup = modal_template.cloneNode(true);
+				document.body.append(markup);
+			}
+		});
+	});
+}
+const promotions = document.querySelector('.promotions');
+promotions.addEventListener('click', (e) => {
+	let target = e.target;
+	if (target.getAttribute('data-target') === 'promotion-modal') {
+		modalGenerator(target);
+		$('#promotion-modal').modal({ keyboard: true });
+	}
+});
+
+
