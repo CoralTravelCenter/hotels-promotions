841a25a8e62420ec37da6eca65245b425f163ece
Mike <50363065+MikeGankin@users.noreply.github.com>
2023-11-15 15:12:29 +0300

    Update 2.0

--- interface.js
+++ interface.js
@@ -5,6 +5,26 @@ const prevBtn = document.querySelector('[data-snap-slider-prev]');
 const locations = [...document.querySelectorAll('[data-location]')];
 const navigation = document.querySelector('.navigation-list');
 const navHeight = document.querySelector('.hotel-promotions nav').clientHeight;
+const browHeight = document.querySelector('[data-module="searchtabsmain"]').clientHeight;
+const height_h4 = document.querySelector('.promotions-list h4').clientHeight;
+const b_crumb = document.querySelector('.bcrumb').clientHeight;
+let scrollPosition = 0;
+
+/*Блокировка скрола на iOS*/
+function enable() {
+	scrollPosition = window.pageYOffset;
+	document.body.style.overflow = 'hidden';
+	document.body.style.position = 'fixed';
+	document.body.style.top = `-${scrollPosition}px`;
+	document.body.style.width = '100%';
+}
+function disable() {
+	document.body.style.removeProperty('overflow');
+	document.body.style.removeProperty('position');
+	document.body.style.removeProperty('top');
+	document.body.style.removeProperty('width');
+	window.scrollTo(0, scrollPosition);
+}
 
 /*Добавляем кнопки в навигацию когда появляется горизонтальный скролл*/
 function addNavigationButtons() {
@@ -38,11 +58,42 @@ prevBtn.addEventListener('click', () => {
 function navScroller(e) {
 	if (e.target.hasAttribute('data-location')) {
 		const dataAttr = e.target.getAttribute('data-location');
-		document.querySelector(`[data-name=${dataAttr}]`).scrollIntoView({behavior: "smooth", block: "nearest", inline: "start"});
+		let scrollPadding = navHeight + browHeight + parseInt(getComputedStyle(document.querySelector('.promotions')).paddingTop, 10) + height_h4 + b_crumb;
+		document.documentElement.style.setProperty('--scroll-padding-top', scrollPadding + "px");
+		document.querySelector(`[data-name=${dataAttr}]`).scrollIntoView(true);
 	}
 }
 navigation.addEventListener('click', navScroller);
 
+/*Удаляем просроченную акцию*/
+const promotions_list = document.querySelectorAll('.promotions-list__item');
 
-		// locations.forEach(element => element.classList.remove('js-active'));
-		// e.target.classList.add('js-active');
+function dateFormatter(str) {
+	const reformat_date = str.split(".").reverse().join("-");
+	return reformat_date;
+}
+
+function promotionTimer(promo) {
+	if (promo.getAttribute('promotion-end') !== '') {
+		const END = promo.getAttribute('promotion-end');
+		const DATE = moment().format('YYYY-MM-DD');
+		if (moment(DATE).isSameOrAfter(dateFormatter(END))) document.querySelector(`[promotion-end="${END}"]`).remove();
+	}
+}
+promotions_list.forEach(promo => promotionTimer(promo));
+
+/*Navigation Observer*/
+const targets = document.querySelectorAll('.promotions-list h4');
+const options = {
+	rootMargin: "-30% 0% -30% 0%",
+	threshold: 0.5
+};
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			locations.forEach(location => location.classList.remove('js-active'));
+			document.querySelector(`[data-location=${entry.target.closest('.promotions-list').getAttribute('data-name')}]`).classList.add('js-active');
+		}
+	});
+}, options);
+targets.forEach(target => observer.observe(target));
---
+++ settings.js
@@ -0,0 +1,111 @@
+const PROMOTION_SETTINGS = [
+	{
+		country_id: 1,
+		name: "ОАЭ",
+		hotels: [
+			{
+				hotel_id: 1,
+				visual: "https://content.coral.ru//resize/800x600/media/image/31/46458/637708480219178761.jpg",
+				rating: 4,
+				hotel_name: "THE FIRST COLLECTION JUMEIRAH VILLAGE CIRCLE",
+				location: "Дубай (Dubai), Аль Барша (Al Barsha)",
+				description: {
+					text: "Возможность ранней регистрации заезда в 8.00 (необходимо уведомить отель за 48 часов до прибытия гостя, а так же сообщить информацию о рейсе и времени прибытия).",
+					list: []
+				},
+				conditions: [
+					"Акция не распространяется на даты: (29.11.2023 – 12.12.2023, 28.12.2023 – 02.01.2024)",
+					"Акция распространяется на бронирование полных пакетных туров",
+					"Не распространяется на групповые заявки от 15 человек",
+					"Стоимость туров в поиске тура обозначена с учетом акции"
+				],
+				booking: "31.05.2024",
+				accommodation: "31.05.2024",
+				departure_from: "Все",
+				url: "https://www.coral.ru/hotels/united-arab-emirates/the-first-collection-jumeirah-village-circle-dubai/",
+				promotion_end: ''
+			}
+		]
+	},
+	{
+		name: "Египет",
+		hotels: [
+			{
+				hotel_id: 2,
+				visual: "https://content.coral.ru//resize/800x600/media/image/12/9270/636751320657246517.jpg",
+				rating: 5,
+				hotel_name: "BARON PALACE RESORT SAHL HASHEESH",
+				location: "Хургада (Hurghada), Сал Хашиш (Salh Hasheesh)",
+				description: {
+					text: "Отдыхай с детьми - плати только за взрослых, двое детей до 14,99 лет – бесплатно!",
+					list: []
+				},
+				conditions: [
+					"Акция распространяется на бронирование полных пакетных туров.",
+					"Акция не распространяется на кроме мастера нестандартного тура и групповые туры от 15 человек.",
+					"Стоимость туров в поиске тура отображается с учетом акции."
+				],
+				booking: "20.12.2023",
+				accommodation: "20.12.2023",
+				departure_from: "Все",
+				url: "https://www.coral.ru/hotels/egypt/baron-palace-resort-sahl-hasheesh-hurghada/",
+				promotion_end: ''
+			},
+			{
+				hotel_id: 3,
+				visual: "https://content.coral.ru//resize/800x600/media/image/12/8871/637795739711641789.jpg",
+				rating: 5,
+				hotel_name: "MOVENPICK WATERPARK RESORT & SPA SOMA BAY",
+				location: "Хургада (Hurghada), Сома-Бэй (Soma Bay)",
+				description: {
+					text: "Пакет СЕМЕЙНЫЙ 2+2=2 (акция для Deluxe rooms - 2 детей до 11, 99 лет бесплатно (в одной комнате с родителями)). АКЦИЯ для ВСЕХ типов номеров - прекрасная возможность отдохнуть в Спа центре и насладиться пакетом услуг по промо цене! В пакет включено:",
+					list: [
+						"сауна (сухой пар) - 15 мин.",
+						"джакузи - 15 мин.",
+						"сауна (хамам) - 15 мин.",
+						"скраб всего тела - 25 мин.",
+						"кокосовая маска на все тело - 25 мин.",
+						"массаж спины - 35 мин."
+					]
+				},
+				conditions: [
+					"Акция распространяется на бронирование полных пакетных туров.",
+					"Акция не распространяется на кроме мастера нестандартного тура и групповые туры от 15 человек.",
+					"Особые условия: Пакет СЕМЕЙНЫЙ 2+2=2 (тип номера Deluxe rooms, возраст детей до 11,99)",
+					"Особые условия: пакет СПА-услуг (для всех типов номеров, на все резервации c 17.10.23)"
+				],
+				booking: "17.10.23",
+				accommodation: "17.10.23",
+				departure_from: "Все",
+				url: "https://www.coral.ru/hotels/egypt/movenpick-waterpark-resort-spa-soma-bay-hurghada/",
+				promotion_end: ''
+			}
+		]
+	},
+	{
+		name: "Таиланд",
+		hotels: [
+			{
+				hotel_id: 4,
+				visual: "https://content.coral.ru//resize/800x600/media/image/33/2262/637720711089614767.jpg",
+				rating: 5,
+				hotel_name: "CENTARA RESERVE SAMUI (ex.CENTARA GRAND BEACH RESORT SAMUI)",
+				location: "о. Самуи (Koh Samui), Чавенг (Chaweng)",
+				description: {
+					text: "Для туров от 7-и ночей, забронированных на следующие даты - предоставляется 1 бесплатный ужин для 2 человек (Thai sharing set) и бесплатный тайский массаж (60 минут / 2 человека)",
+					list: []
+				},
+				conditions: [
+					"Акция не распространяется на даты: (01.01.2024-07.01.2024, 08.02.2024-18.02.2024, 29.03.2024-31.03.2024)",
+					"Акция не распространяется на забронированные через Мастера нестандартного тура, и на групповые туры от 15 человек",
+					"Стоимость туров в поиске тура обозначена с учетом акции"
+				],
+				booking: "31.12.2023",
+				accommodation: "30.06.2024",
+				departure_from: "Все",
+				url: "https://www.coral.ru/hotels/thailand/centara-reserve-samui-ex-centara-grand-beach-resort-samui-koh-samui/",
+				promotion_end: ''
+			}
+		]
+	},
+];
