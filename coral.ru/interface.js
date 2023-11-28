52d0c82e8cdc77cc7c723d970667620b41005a18
Mike <mikhail-gankin@yandex.ru>
2023-11-28 21:54:41 +0300

    Refactoring

--- coral.ru/interface.js
+++ coral.ru/interface.js
@@ -1,3 +1,4 @@
+/*Переменные*/
 const snapSlider = document.querySelector('[data-snap-slider]');
 const snapSlides = [...document.querySelectorAll('.navigation-list__item')];
 const nextBtn = document.querySelector('[data-snap-slider-next]');
@@ -10,45 +11,53 @@ const height_h4 = document.querySelector('.promotions-list h4').clientHeight;
 // const b_crumb = document.querySelector('.bcrumb').clientHeight;
 let scrollPosition = 0;
 
-/*Блокировка скрола на iOS*/
-function disableScroll() {
-	scrollPosition = window.pageYOffset;
-	document.body.style.overflow = 'hidden';
-	document.body.style.position = 'fixed';
-	document.body.style.top = `-${scrollPosition}px`;
-	document.body.style.width = '100%';
-}
-function enableScroll() {
-	document.body.style.removeProperty('overflow');
-	document.body.style.removeProperty('position');
-	document.body.style.removeProperty('top');
-	document.body.style.removeProperty('width');
-	window.scrollTo(0, scrollPosition);
-}
-
-const promotions = document.querySelector('section.hotel-promotions .promotions');
-promotions.childNodes.forEach(node => {
-	if (node.nodeType === 3) node.remove();
-});
-promotions.addEventListener('click', (e) => {
-	let target = e.target;
-	if (target.getAttribute('data-target') === 'promotion-modal') {
-		modalGenerator(target);
-		if (document.querySelector('#promotion-modal')) {
-			$('#promotion-modal').modal({ keyboard: true });
-			$('#promotion-modal').on('shown.bs.modal', () => {
-				if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) disableScroll();
-			});
-			$('#promotion-modal').on('hidden.bs.modal', () => {
-				$('#promotion-modal').remove();
-				if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) enableScroll();
-			});
-		}
+/*Блокирвка скрола на iOS*/
+const disablePageScroll = () => {
+	if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+		scrollPosition = window.pageYOffset;
+		document.body.style.overflow = 'hidden';
+		document.body.style.position = 'fixed';
+		document.body.style.top = `-${scrollPosition}px`;
+		document.body.style.width = '100%';
 	}
-});
+};
+const enablePageScroll = () => {
+	if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+		document.body.style.removeProperty('overflow');
+		document.body.style.removeProperty('position');
+		document.body.style.removeProperty('top');
+		document.body.style.removeProperty('width');
+		window.scrollTo(0, scrollPosition);
+	}
+};
+
+/*Рендер модального окна*/
+const modalRender = () => {
+	const promotions = document.querySelector('section.hotel-promotions .promotions');
+	promotions.childNodes.forEach(node => {
+		if (node.nodeType === 3) node.remove();
+	});
+	promotions.addEventListener('click', (e) => {
+		let target = e.target;
+		if (target.getAttribute('data-target') === 'promotion-modal') {
+			modalGenerator(target);
+			if (document.querySelector('#promotion-modal')) {
+				$('#promotion-modal').modal({ keyboard: true });
+				$('#promotion-modal').on('shown.bs.modal', () => {
+					if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) disableScroll();
+				});
+				$('#promotion-modal').on('hidden.bs.modal', () => {
+					$('#promotion-modal').remove();
+					if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) enableScroll();
+				});
+			}
+		}
+	});
+};
+modalRender();
 
 /*Добавляем кнопки в навигацию когда появляется горизонтальный скролл*/
-function addNavigationButtons() {
+const renderNavigationButtons = () => {
 	if (snapSlider.clientWidth < snapSlider.scrollWidth) {
 		nextBtn.style.display = 'block';
 		prevBtn.style.display = 'block';
@@ -56,106 +65,121 @@ function addNavigationButtons() {
 		nextBtn.style.display = 'none';
 		prevBtn.style.display = 'none';
 	}
-}
-addNavigationButtons();
-window.addEventListener('resize', _.throttle(addNavigationButtons, 1000));
+};
+window.addEventListener('resize', _.throttle(renderNavigationButtons, 500));
 
-nextBtn.addEventListener('click', () => {
-	snapSlides[snapSlides.length - 1].scrollIntoView({
-		behavior: 'smooth',
-		inline: 'center',
-		block: 'nearest'
+/*Логика кнопок навигации*/
+const navigationButtonsClicker = () => {
+	nextBtn.addEventListener('click', () => {
+		snapSlides[snapSlides.length - 1].scrollIntoView({
+			behavior: 'smooth',
+			inline: 'center',
+			block: 'nearest'
+		});
 	});
-});
-prevBtn.addEventListener('click', () => {
-	snapSlides[0].scrollIntoView({
-		behavior: 'smooth',
-		inline: 'center',
-		block: 'nearest'
+	prevBtn.addEventListener('click', () => {
+		snapSlides[0].scrollIntoView({
+			behavior: 'smooth',
+			inline: 'center',
+			block: 'nearest'
+		});
 	});
-});
+};
+navigationButtonsClicker();
 
 /*Навигация*/
-function navScroller(e) {
+const navScrollHandler = (e) => {
 	if (e.target.hasAttribute('data-location')) {
-		const dataAttr = e.target.getAttribute('data-location');
-		let scrollPadding = navHeight + browHeight + parseInt(getComputedStyle(document.querySelector('.promotions')).paddingTop, 10) + height_h4 + b_crumb;
+		const target = e.target.getAttribute('data-location');
+		const scrollPadding = navHeight + browHeight + parseInt(getComputedStyle(document.querySelector('.promotions')).paddingTop, 10) + height_h4 + b_crumb;
 		document.documentElement.style.setProperty('--scroll-padding-top', scrollPadding + "px");
-		document.querySelector(`[data-name=${dataAttr}]`).scrollIntoView({
+		document.querySelector(`[data-name=${target}]`).scrollIntoView({
 			behavior: 'smooth',
 			block: 'start',
+			inline: 'nearest'
 		});
 	}
-}
-navigation.addEventListener('click', navScroller);
+};
+navigation.addEventListener('click', navScrollHandler);
 
 /*Удаляем просроченную акцию*/
-const promotions_list = document.querySelectorAll('.promotions-list__item');
+const expiredPromotionRemove = () => {
+	const promotions_list = document.querySelectorAll('.promotions-list__item');
 
-function dateFormatter(str) {
-	const reformat_date = str.split(".").reverse().join("-");
-	return reformat_date;
-}
-
-function promotionTimer(promo) {
-	if (promo.getAttribute('promotion-end') !== '') {
-		const END = promo.getAttribute('promotion-end');
-		const DATE = moment().format('YYYY-MM-DD');
-		if (moment(DATE).isSameOrAfter(dateFormatter(END))) document.querySelector(`[promotion-end="${END}"]`).remove();
+	function dateFormatter(str) {
+		const reformat_date = str.split(".").reverse().join("-");
+		return reformat_date;
 	}
-}
-promotions_list.forEach(promo => promotionTimer(promo));
 
-/*Navigation Observer*/
-const targets = document.querySelectorAll('.promotions-list h4');
-const options = {
-	rootMargin: "-30% 0% -30% 0%",
-	threshold: 0.5
-};
-const observer = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			locations.forEach(location => location.classList.remove('js-active'));
-			document.querySelector(`[data-location=${entry.target.closest('.promotions-list').getAttribute('data-name')}]`).classList.add('js-active');
+	function promotionTimer(promo) {
+		if (promo.getAttribute('promotion-end') !== '') {
+			const END = promo.getAttribute('promotion-end');
+			const DATE = moment().format('YYYY-MM-DD');
+			if (moment(DATE).isSameOrAfter(dateFormatter(END))) document.querySelector(`[promotion-end="${END}"]`).remove();
 		}
-	});
-}, options);
-targets.forEach(target => observer.observe(target));
+	}
+	promotions_list.forEach(promo => promotionTimer(promo));
+};
+expiredPromotionRemove();
 
-
-/*Touch gesters*/
-const mobileWidthMediaQuery = window.matchMedia('(max-width: 576px)');
-let swipe_target;
-function mobileChecker(isMobileSize) {
-	if (isMobileSize) {
-		document.body.addEventListener('swiped-down', function (e) {
-			if (e.target.classList.contains('custom-modal')) {
-				$('#promotion-modal').modal('hide');
-				$('#promotion-modal').on('hidden.bs.modal', () => {
-					$('#promotion-modal').remove();
-					if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) enableScroll();
-				});
+/*Переключаем активную кнопку по скролу*/
+const navigationObserver = () => {
+	const targets = document.querySelectorAll('.promotions-list h4');
+	const options = {
+		rootMargin: "-25% 0% -25% 0%",
+		threshold: 0.5
+	};
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				locations.forEach(location => location.classList.remove('js-active'));
+				document.querySelector(`[data-location=${entry.target.closest('.promotions-list').getAttribute('data-name')}]`).classList.add('js-active');
 			}
 		});
+	}, options);
+	targets.forEach(target => observer.observe(target));
+};
+navigationObserver();
 
-		/*Mutation Observer*/
-		const items = document.querySelectorAll(".navigation-list__item button");
-		function animationObserver(el) {
-			el.addEventListener('transitionend', () => {
-				if (el.classList.contains('js-active')) {
-					el.scrollIntoView({
-						behavior: 'smooth',
-						inline: 'center',
-						block: 'nearest'
-					});
-				}
+
+/*Скрываем модальное окно по свайпу вниз*/
+const createTouchEvent = () => {
+	document.body.addEventListener('swiped-down', function (e) {
+		if (e.target.classList.contains('custom-modal')) {
+			$('#promotion-modal').modal('hide');
+			$('#promotion-modal').on('hidden.bs.modal', () => {
+				$('#promotion-modal').remove();
+				if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) enableScroll();
 			});
 		}
-		items.forEach(el => animationObserver(el));
+	});
+};
+
+/*Прокручиваем навигацию чтобы было видно активную кнопку*/
+const mutationObserver = () => {
+	const scroll_container = document.querySelector('.navigation-list');
+	const items = document.querySelectorAll(".navigation-list__item button");
+	function animationObserver(el) {
+		el.addEventListener('transitionend', () => {
+			if (el.classList.contains('js-active')) {
+				scroll_container.scrollTo({ left: el.offsetLeft, behavior: 'smooth' });
+				// scroll_container.scrollTo({ right: el.offsetRight, behavior: 'smooth'});
+			}
+		});
+	}
+	items.forEach(el => animationObserver(el));
+};
+
+/*Запускаем неоходимый код на телефонах*/
+const mobileWidthMediaQuery = window.matchMedia('(max-width: 576px)');
+function mobileChecker(isMobileSize) {
+	if (isMobileSize) {
+		createTouchEvent();
+		mutationObserver();
 	}
 }
 mobileChecker(mobileWidthMediaQuery.matches);
-mobileWidthMediaQuery.addEventListener('change', function (e) {
+mobileWidthMediaQuery.addEventListener('change', (e) => {
 	mobileChecker(e.matches);
 });
 
